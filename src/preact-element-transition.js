1b23feb83e6db71e79ed48bb21445c0091ea1351
Michael "Z" Goddard <mzgoddard@gmail.com>
2017-08-29 14:19:12 -0400

    fixup! Add Preact classes

--- src/preact-element-transition.js
+++ src/preact-element-transition.js
@@ -7,16 +7,45 @@ class PreactElementTransition extends BaseTransition {
     super(bus, tree, matcher);
     this.loop = loop || RunLoop.main;
 
-    this.stateChange = this.bus.bind('state:change', 3);
+    this.alive = {};
+    this.left = {};
 
+    this.stateChange = this.bus.bind('state:change', 3);
+    this.stateDestroy = this.bus.bind('state:destroy', 2);
+
+    this.bus.on('state:begin', this.onStateBegin.bind(this));
     this.bus.on('state:end', this.onStateEnd.bind(this));
     this.bus.on('element:create', this.onElementCreate.bind(this));
     this.bus.on('element:update', this.onElementUpdate.bind(this));
+    this.bus.on('element:destroy', this.onElementDestroy.bind(this));
+  }
+
+  onStateBegin(type, id, animation) {
+    if (this.alive[id]) {
+      this.alive[id](true);
+    }
   }
 
   onStateEnd(type, id, animation) {
     if (animation === 'leave' && this.tree.element(id)) {
-      this.tree.element(id).remove(id);
+      Promise.race([
+        new Promise(resolve => {
+          this.alive[id] = resolve;
+        }),
+        this.loop.soon()
+        .then(() => this.loop.soon())
+        .then(() => this.loop.soon())
+        .then(() => this.loop.soon())
+        .then(() => this.loop.soon())
+        .then(() => false),
+      ])
+      .then(alive => {
+        this.alive[id] = null;
+        if (!alive) {
+          this.tree.element(id).remove(id);
+          this.stateDestroy(type, id);
+        }
+      });
     }
   }
 
@@ -26,16 +55,14 @@ class PreactElementTransition extends BaseTransition {
       const path = branch.path;
       const meta = branch.meta(id);
       meta.type = type;
-      meta.can = this.matcher.results[type].hasAnimation
-      if (!this.didEnter(path, id) && this.canEnter(path, id)) {
+      meta.can = this.matcher.results[type].hasAnimation;
+      if (!this.didEnter(path, id) && this.canEnter(path, id) && branch.count > 1) {
         const _className = element.className;
         element.className = `${_className} enter`;
-        branch.meta(id).didEnter = true;
-        this.loop.soon().then(() => {
-          element.className = _className;
-          this.stateChange(type, id, 'enter');
-        });
+        element.className = _className;
+        this.stateChange(type, id, 'enter');
       }
+      branch.meta(id).didEnter = true;
     }
   }
 
@@ -43,13 +70,40 @@ class PreactElementTransition extends BaseTransition {
     if (this.tree.element(id)) {
       const branch = this.tree.element(id);
       const meta = branch.meta(id);
+      meta.can = this.matcher.results[type].hasAnimation;
       if (meta.didLeave && !meta.leaving) {
+        meta._leaving = element.className;
         meta.leaving = `${element.className} leave`;
+        this.left[id] = [meta.leaving, element.className];
+      }
+      if (meta.didLeave && meta.leaving) {
+        element.className = meta.leaving;
         this.stateChange(type, id, 'leave');
       }
-      if (meta.leaving) {
-        element.className = meta.leaving;
+      if (!meta.didLeave && this.left[id]) {
+        if (element.className === this.left[id][0]) {
+          element.className = this.left[id][1];
+        }
+        this.left[id] = null;
+        meta._leaving = null;
+        meta.leaving = null;
       }
+      if (!this.didEnter(branch.path, id) && this.canEnter(branch.path, id) && branch.count > 1) {
+        const _className = element.className;
+        element.className = `${_className} enter`;
+        element.className = _className;
+        this.stateChange(type, id, 'enter');
+      }
+      branch.meta(id).didEnter = true;
+    }
+  }
+
+  onElementDestroy(type, id, element) {
+    if (this.left[id]) {
+      this.left[id] = null;
+    }
+    if (this.tree.element(id)) {
+      this.tree.element(id).remove(id);
     }
   }
 }
