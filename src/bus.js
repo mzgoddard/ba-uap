922b60844bf2d7343396ee24c1919803c989a264
Michael "Z" Goddard <mzgoddard@gmail.com>
2017-08-30 17:17:06 -0400

    fixup! Add Bus

--- src/bus.js
+++ src/bus.js
@@ -1,3 +1,24 @@
+class BusSet {
+  constructor() {
+    this.set = [];
+  }
+
+  values() {
+    return this.set;
+  }
+
+  add(value) {
+    this.set.push(value);
+  }
+
+  delete(value) {
+    const index = this.set.indexOf(value);
+    if (index !== -1) {
+      this.set.splice(index, 1);
+    }
+  }
+}
+
 class Bus {
   constructor() {
     this.bound = {};
@@ -7,10 +28,10 @@ class Bus {
   setFor(a, b) {
     this.sets[a] = this.sets[a] || {};
     if (b === '*') {
-      return this.sets[a].__all__ = this.sets[a].__all__ || new Set();
+      return this.sets[a].__all__ = this.sets[a].__all__ || new BusSet();
     }
     else {
-      return this.sets[a][b] = this.sets[a][b] || new Set();
+      return this.sets[a][b] = this.sets[a][b] || new BusSet();
     }
   }
 
@@ -22,50 +43,52 @@ class Bus {
 
       if (n === 1) {
         this.bound[s] = v => {
-          for (const item of all) {
+          for (const item of all.values()) {
             item(b, v);
           }
-          for (const item of set) {
+          for (const item of set.values()) {
             item(v);
           }
         };
       }
       else if (n === 2) {
         this.bound[s] = (x, y) => {
-          for (const item of all) {
+          for (const item of all.values()) {
             item(b, x, y);
           }
-          for (const item of set) {
+          for (const item of set.values()) {
             item(x, y);
           }
         };
       }
       else if (n === 3) {
         this.bound[s] = (x, y, z) => {
-          for (const item of all) {
-            item(b, x, y, z);
+          let values = all.values();
+          for (let i = 0, l = values.length; i < l; ++i) {
+            values[i](b, x, y, z);
           }
-          for (const item of set) {
-            item(x, y, z);
+          values = set.values();
+          for (let i = 0, l = values.length; i < l; ++i) {
+            values[i](x, y, z);
           }
         };
       }
       else if (n === 4) {
         this.bound[s] = (w, x, y, z) => {
-          for (const item of all) {
+          for (const item of all.values()) {
             item(b, w, x, y, z);
           }
-          for (const item of set) {
+          for (const item of set.values()) {
             item(w, x, y, z);
           }
         };
       }
       else {
         this.bound[s] = (...args) => {
-          for (const item of all) {
+          for (const item of all.values()) {
             item(b, ...args);
           }
-          for (const item of set) {
+          for (const item of set.values()) {
             item(...args);
           }
         };
