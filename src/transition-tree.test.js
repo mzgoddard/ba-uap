f15db43397489a54af97fc75e46d871e9911ff87
Michael "Z" Goddard <mzgoddard@gmail.com>
2017-08-30 09:32:45 -0400

    fixup! Add BaseTransition and TransitionTree

--- src/transition-tree.test.js
+++ src/transition-tree.test.js
@@ -44,37 +44,41 @@ it('get path returns list', () => {
 
 it('element id refers to parent list', () => {
   const branch = tree.get('root');
+  const current = [];
   const missed = [];
-  branch.update([Node.e('first'), Node.e('second')], missed);
+  branch.update([Node.e('first'), Node.e('second')], current, missed);
   expect(tree.element('first')).toBe(branch);
 });
 
 it('stores list', () => {
   const branch = tree.get('root');
+  const current = [];
   const missed = [];
   const first = Node.e('first');
   const second = Node.e('second');
-  branch.update([first, second], missed);
+  branch.update([first, second], current, missed);
   expect(branch.nodes['first']).toBe(first);
-  branch.update([first, Node.e('second')], missed);
+  branch.update([first, Node.e('second')], current, missed);
   expect(branch.nodes['second']).not.toBe(second);
   expect(branch.nodes['second']).toEqual(second);
 });
 
 it('updating sets list of missed items', () => {
   const branch = tree.get('root');
+  const current = [];
   const missed = [];
-  branch.update([Node.e('first'), Node.e('second')], missed);
-  branch.update([Node.e('second')], missed);
+  branch.update([Node.e('first'), Node.e('second')], current, missed);
+  branch.update([Node.e('second')], current, missed);
   expect(missed).toEqual(['first']);
 });
 
 it('inserts missing items into dest', () => {
   const branch = tree.get('root');
+  const current = [];
   const missed = [];
   const first = Node.e('first');
-  branch.update([first, Node.e('second')], missed);
-  branch.update([Node.e('second')], missed);
+  branch.update([first, Node.e('second')], current, missed);
+  branch.update([Node.e('second')], current, missed);
   const dest = [];
   branch.missedNodes(null, dest);
   expect(dest.length).toBe(2);
@@ -83,17 +87,19 @@ it('inserts missing items into dest', () => {
 
 it('removes items', () => {
   const branch = tree.get('root');
+  const current = [];
   const missed = [];
-  branch.update([Node.e('first'), Node.e('second')], missed);
+  branch.update([Node.e('first'), Node.e('second')], current, missed);
   branch.remove('first');
-  branch.update([Node.e('second')], missed);
+  branch.update([Node.e('second')], current, missed);
   expect(missed).toEqual([]);
 });
 
 it('removes item meta', () => {
   const branch = tree.get('root');
+  const current = [];
   const missed = [];
-  branch.update([Node.e('first'), Node.e('second')], missed);
+  branch.update([Node.e('first'), Node.e('second')], current, missed);
   branch.meta('first').isMeta = true;
   expect(branch._meta['first']).toEqual({isMeta: true});
   branch.remove('first');
@@ -102,8 +108,9 @@ it('removes item meta', () => {
 
 it('removes child list', () => {
   const branch = tree.get('root');
+  const current = [];
   const missed = [];
-  branch.update([Node.e('first'), Node.e('second')], missed);
+  branch.update([Node.e('first'), Node.e('second')], current, missed);
   tree.get('root.first');
   branch.remove('first');
   expect(tree.lists['root.first']).toBeFalsy();
@@ -111,20 +118,9 @@ it('removes child list', () => {
 
 it('removes connection to parent list for elements', () => {
   const branch = tree.get('root');
+  const current = [];
   const missed = [];
-  branch.update([Node.e('first'), Node.e('second')], missed);
+  branch.update([Node.e('first'), Node.e('second')], current, missed);
   branch.remove('first');
   expect(tree.element('first')).toBeFalsy();
-});
-
-it('storing a list with a node missing an id clears the list', () => {
-  const branch = tree.get('root');
-  const missed = [];
-  branch.update([Node.e('first'), Node.e('second')], missed);
-  branch.update([Node.u()], missed);
-  expect(missed.length).toBe(0);
-  expect(branch.nodes['first']).toBeFalsy();
-  // Update so branch can prime its order array
-  branch.update([], missed);
-  expect(branch.order.length).toBe(0);
 });
